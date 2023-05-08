14341729857cd5f09c692ad9cdc45075dff105bc
Jarrod Kohl <jarrod.kohl@gmail.com>
2023-05-08 13:23:10 -0400

    Styling

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,10 +1,21 @@
 export const AppLayout = ({ children }) => {
   return(
-    <div>
-      <h1>This is the App Layout</h1>
-      <div>
-        {children}
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-slate-600">
+          <div>LOGO</div>
+          <div>CTA buttons</div>
+          <div>Tokens</div>
+        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-600 to-cyan-300">
+          List of Posts
+        </div>
+        <div className="bg-cyan-300">
+          <div>User info - logout button</div>
+        </div>
+
       </div>
+      <div className="bg-slate-100">{children}</div>
     </div>
 
   )
