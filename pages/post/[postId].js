87df20bae8e7a478fc7fd85f7e28bb4ebba0e862
Jarrod Kohl <jarrod.kohl@gmail.com>
2023-05-13 17:23:53 -0400

    sidebar

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,68 +1,42 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { ObjectId } from "mongodb";
-import { AppLayout } from "../../components/AppLayout";
-import clientPromise from "../../lib/mongodb";
+// 
 
-export default function Post(props) {
-  console.log(props)
-  console.log(props.postContent)
-  console.log('post id', props.postId)
-  
-  
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+import { AppLayout } from '../../components/AppLayout'
 
-  
+export default function Post() {
+  const router = useRouter()
+  const { postId } = router.query
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    if (postId) {
+      fetch(`/api/post/${postId}`)
+        .then((res) => res.json())
+        .then((json) => setPost(json.post))
+        .catch((err) => console.log(err))
+    }
+  }, [postId])
+
+  if (!post) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div className="overflow-auto h-full">
-      <div className="max-w-screen-sm mx-auto">
-        <div className="text-sm font-bold mt-6 p-2 bg-slate-100 rounded-sm">
-          Your Post Content
-        </div>
-        <div className="p-2 bg-slate-100 rounded-sm">
-          {props.postContent}
-
-        </div>
-
-      </div>
-      
+    <div>
+      <h1>Post {postId}</h1>
+      <p>{post.postContent}</p>
     </div>
   )
+  
 }
-
 Post.getLayout = function getLayout(page, pageProps) {
   return <AppLayout {...pageProps}>{page}</AppLayout>;
 };
 
-export const getServerSideProps = withPageAuthRequired(async (context) => {
-    const userSession = await getSession(context.req, context.res)
-    const client = await clientPromise
-    const db = await client.db("ContentAgent")
-    const userProfile = await db.collection("users").findOne({
-      auth0Id: userSession.user.sub,
-    })
-    const post = await db.collection("posts").findOne({
-      _id: ObjectId(context.query.postId),
-      userId: userProfile._id,  // should this be userId: user._id
-    })
-    console.log('context', context.query)
-    console.log('post', post)
-
-    if(!post) {
-      return {
-        redirect: {
-          destination: "/post/new",
-          permanent: false,
-        }
-      }
-    }
-    
-
-    return {
-      props: {
-        postContent: post.postContent,
-        title: post.title,
-        keywords: post.keywords,
-        metaDescription: post.metaDescription,
-        postId: context.params.postId,
-      }
-    }
+export const getServerSideProps = withPageAuthRequired (() => {
+  return {
+    props: {},
+  };
 })
